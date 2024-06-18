e33804133d4a5b8ac111970029de1d21b0040b13
--global <lucascherez.cl@gmail.com>
2024-06-18 15:26:55 -0300

    Migrations

--- Rotas-e-HTTP/02-api-rest-nodejs/src/server.ts
+++ Rotas-e-HTTP/02-api-rest-nodejs/src/server.ts
@@ -1,15 +1,25 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import fastify from 'fastify'
-
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transaction = await knex('transactions').insert({
+    id: crypto.randomUUID(),
+    title: 'Transação de teste',
+    amount: 55,
+  })
 
-  return tables
+  return transaction
+})
+
+app.get('/transaction', async () => {
+  const transactions = await knex('transactions').select('*')
+
+  return transactions
 })
 
 app
