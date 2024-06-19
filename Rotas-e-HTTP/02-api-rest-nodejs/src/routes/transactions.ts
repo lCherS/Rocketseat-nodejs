98b7669df6d7f28e873f167b4c0bcb0272b17209
--global <lucascherez.cl@gmail.com>
2024-06-19 10:43:37 -0300

    separando rota e criando transação

---
+++ Rotas-e-HTTP/02-api-rest-nodejs/src/routes/transactions.ts
@@ -0,0 +1,26 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import crypto from 'node:crypto'
+import { knex } from '../database'
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.post('/', async (req, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(req.body)
+
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
+  })
+}
--- Rotas-e-HTTP/02-api-rest-nodejs/src/server.ts
+++ Rotas-e-HTTP/02-api-rest-nodejs/src/server.ts
@@ -1,21 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import fastify from 'fastify'
-import crypto from 'node:crypto'
 import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transaction = await knex('transactions').insert({
-    id: crypto.randomUUID(),
-    title: 'Transação de teste',
-    amount: 55,
-  })
-
-  return transaction
-})
+app.register(transactionsRoutes)
 
 app.get('/transaction', async () => {
   const transactions = await knex('transactions').select('*')
